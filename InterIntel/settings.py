429652615e1d2220cccc93c173bc746ad1c1dc34
collinsbett29 <collinsbegtt29@gmail.com>
2021-03-11 10:48:39 +0300

    Added static file configuration for bootstrap files

--- InterIntel/settings.py
+++ InterIntel/settings.py
@@ -40,8 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',    
     'bootstrap4',
-
-
 ]
 
 MIDDLEWARE = [
@@ -121,7 +119,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
